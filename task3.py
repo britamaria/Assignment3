7226f02741eb9b12dbf7c9b38c9222c9a5ce0835
britamaria <s341553@oslomet.no>
2021-04-29 17:59:19 +0200

    test

--- task3.py
+++ task3.py
@@ -1,45 +1,15 @@
-import numpy as np
-import matplotlib.pyplot as plt 
-import diceRoll as dr
-import judger as jg
+from diceRoll import Roll
+from judger import judge
 
 
 
 options=["Player1: Roll the dice", "Player2: Roll the dice"]
 
 
-
-player1 = diceRoll()
-player2 = diceRoll()
-
-# print("player1:", player1)
-# print("player2:", player2)
-
-# def judger(opt1,opt2): 
-# #   utelukke feil input og uavgjort
-#     if opt1 not in options or opt2 not in options:
-#         return -1
-# #   uavgjot
-#     if opt1 == opt2:
-#         return 0
-    
-#     index1 = options.index(opt1)
-#     index2 = options.index(opt2)
-    
-#     if(index1 + 1 == index2):
-#         return 2
-# #   len(options -1 vil være lik 2, eller den siste indexen)
-#     elif(index1==len(options)-1 and index2== 0):
-#         return 2
-#     else: 
-#         return 1
-    
-
-    
-    
-Player1 = diceRoll()
-Player2 = diceRoll()
-result = Judger(player1, player2)
+  
+player1 = Roll()
+player2 = judge()
+result = judge(player1, player2)
 print(player1,player2,result)
 
 def simulateGame(nGames):
@@ -48,11 +18,11 @@ def simulateGame(nGames):
     tie = 0
     
     for i in range(nGames):
-        roll1 = diceRoll()
-        roll2 = diceRoll()
+        roll1 = Roll()
+        roll2 = Roll()
         print("player 1 rolls")
         print("player 2 rolls")
-        result = Judger(roll1,roll2)
+        result = judge(roll1,roll2)
         if(result == -1):
             print("Wrong input")
         elif(result == 0):
@@ -73,3 +43,6 @@ def simulateGame(nGames):
 
 a = simulateGame(1)
 print(a)
+
+
+
