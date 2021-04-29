ad3f003f11ae7fb989a7d70a9b94643a007f4f20
britamaria <s341553@oslomet.no>
2021-04-29 17:23:42 +0200

    dice

--- diceRoll.py
+++ diceRoll.py
@@ -3,8 +3,5 @@ import numpy as np
 
 def diceRoll():
     die1 = np.random.randint(1,6)
-    die2 = np.random.randint(1,6)
-    die3 = np.random.randint(1,6)
-    die4 = np.random.randint(1,6)
-    return [die1, die2, die3, die4]
+    return die1
     
